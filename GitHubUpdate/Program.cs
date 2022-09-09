cf79e94c01f32696a446991405d80124cb56bfcc
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-09-09 16:37:53 -0700

    'update'

--- GitHubUpdate/Program.cs
+++ GitHubUpdate/Program.cs
@@ -13,10 +13,35 @@ namespace GitHubUpdate
     {
         static void run_git_add(string path)
         {
-            Process p = new Process();
+            Process cmdProcess = new Process();
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = "C:\\Windows\\System32\\cmd.exe";
+            processStartInfo.WorkingDirectory = path;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.RedirectStandardInput = true;
+
+            cmdProcess.StartInfo = processStartInfo;
+            cmdProcess.Start();
+            StreamWriter writer = cmdProcess.StandardInput;
+            writer.WriteLine("git add .");
+            writer.WriteLine("git commit -m 'update'");
+            writer.WriteLine("git push");
+
+            cmdProcess.WaitForExit();
+            Process p = new Process{
+                StartInfo = 
+                {
+                    FileName =  "C:\\Windows\\System32\\cmd.exe",
+                    WorkingDirectory = path,
+                    Arguments = "git add .",
+                    UseShellExecute = true
+                }
+            };
+            p.Start();
             p.StartInfo.WorkingDirectory = path;
             p.StartInfo.FileName = "C:\\Windows\\System32\\cmd.exe";
             p.StartInfo.Arguments = "git add .";
+            p.StartInfo.UseShellExecute = false;
             p.Start();
             p.StartInfo.Arguments = "git commit -m 'Evening auto-push'";
             p.Start();
@@ -26,12 +51,13 @@ namespace GitHubUpdate
         static void Main(string[] args)
         {
             string file_path = @".\Paths.txt";
+            int hour = 16;
             bool ran_evening = false;
             bool ran_morning = false;
             bool run_evening = false;
             bool run_morning = false;
             DateTime now = DateTime.Now;
-            if (now.Hour < 20)
+            if (now.Hour < hour)
             {
                 run_evening = true;
             }
@@ -41,7 +67,7 @@ namespace GitHubUpdate
                 while (true)
                 {
                     now = DateTime.Now;
-                    if (now.Hour > 20)
+                    if (now.Hour >= hour)
                     {
                         if (!ran_evening)
                         {
@@ -53,9 +79,10 @@ namespace GitHubUpdate
                                 }
                                 foreach (string p in Directory.EnumerateDirectories(path))
                                 {
-                                    run_git_add(p);
+                                    run_git_add(@"C:\\Users\\b5anderson\\Modular_Projects\\GitHubUpdater\\");
                                 }
                             }
+                            ran_evening = true;
                         }
                     }
 
